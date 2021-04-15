aa49a0ae0ed46c655fd36f85bd08b3706d40d9c9
nickr <nicholaspriegel@gmail.com>
2021-04-15 20:31:21 +0200

    .

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ export default function App() {
 	const [piclist, setPiclist] = useState(PicList)
 	const [score, setScore] = useState(0)
 
+	const shuffle = (x) => {
+		let piclist0 = [...x]
+		let piclist1 = []
+		while (piclist0.length > 0) {
+			let i = Math.floor(Math.random() * piclist0.length)
+			piclist1.push(...piclist0.splice(i, 1))
+		}
+		return piclist1
+	}
+
 	const handleClick = (e) => {
 		if (piclist.find(x => x.name === e.target.name).clicked === false) {
 			const newPiclist = piclist.map(x => x.id === parseInt(e.target.id) ? { ...x, clicked: true } : x)
@@ -16,8 +26,10 @@ export default function App() {
 			console.log('YOU LOSE!');
 		}
 	}
+	
+	let picArray = shuffle(piclist)
 
-	let images = piclist.map(({ id, url, name }) => (
+	let images = picArray.map(({ id, url, name }) => (
 		<img
 			key={id}
 			id={id}
@@ -30,7 +42,7 @@ export default function App() {
 
 	return (
 		<div>
-			<h2>Score: { score }</h2>
+			<h2>Score: {score}</h2>
 			{images}
     	</div>
   	)
--- src/PicList.js
+++ src/PicList.js
@@ -10,6 +10,48 @@ const PicList = [
         url: './pictures/Anubis.png',
         name: 'Anubis',
         clicked: false,
-    }
+    },
+    {
+        id: 3,
+        url: './pictures/Bast.png',
+        name: 'Bast',
+        clicked: false,
+    },
+    {
+        id: 4,
+        url: './pictures/Horus.png',
+        name: 'Horus',
+        clicked: false,
+    },
+    {
+        id: 5,
+        url: './pictures/Isis.png',
+        name: 'Isis',
+        clicked: false,
+    },
+    {
+        id: 6,
+        url: './pictures/Osiris.png',
+        name: 'Osiris',
+        clicked: false,
+    },
+    {
+        id: 7,
+        url: './pictures/Ra.png',
+        name: 'Ra',
+        clicked: false,
+    },
+    {
+        id: 8,
+        url: './pictures/Sekhmet.png',
+        name: 'Sekhmet',
+        clicked: false,
+    },
+    {
+        id: 9,
+        url: './pictures/Thoth.png',
+        name: 'Thoth',
+        clicked: false,
+    },
 ]
 export default PicList
